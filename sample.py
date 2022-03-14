9abc2a9baf74d20dd596dd5bd9f469b751a27a46
Ewa Komkowska <komcia1234@gmail.com>
2022-03-14 17:35:39 +0100

    Too much classes in one

--- sample.py
+++ sample.py
@@ -3,22 +3,36 @@ from pathlib import Path
 
 import prodigy
 import spacy
-from prodigy.models.ner import EntityRecognizer
+from brat_parser import get_entities_relations_attributes_groups
+
+
+def read_entities():
+    entities_list, relations_list = [], []
+
+    for i in range(300):
+        entities, relations, _, _ = get_entities_relations_attributes_groups(r".\0-1-2\{0}.ann".format(i))
+        entities_list.append(entities)
+        relations_list.append(relations)
+
+    return entities_list
 
 
 @prodigy.recipe(
     "entity_linker.manual",
     dataset=("The dataset to use", "positional", None, str),
-    source=("The source data as a .txt file", "positional", None, Path),
+    source_dir=("The source dir", "positional", None, Path),
+    recipe_number=("The number of reading recipe using to file name and index of list", "positional", None, int),
     nlp_dir=("Path to the NLP model with a pretrained NER component", "positional", None, Path),
     kb_loc=("Path to the KB", "positional", None, Path),
     entity_loc=("Path to the file with additional information about he entities", "positional", None, Path),
 )
-def entity_linker_manual(dataset, source, nlp_dir, kb_loc, entity_loc):
+def entity_linker_manual(dataset, source_dir, recipe_number, nlp_dir, kb_loc, entity_loc):
     nlp = spacy.load(nlp_dir)
     kb = spacy.kb.KnowledgeBase(vocab=nlp.vocab, entity_vector_length=1)
     kb.from_disk(kb_loc)
-    model = EntityRecognizer(nlp)
+    # model = EntityRecognizer(nlp)
+
+    entities_list = read_entities()
 
     id_dict = {}
     with entity_loc.open("r", encoding="utf8") as csvfile:
@@ -26,9 +40,17 @@ def entity_linker_manual(dataset, source, nlp_dir, kb_loc, entity_loc):
         for row in csvreader:
             id_dict[row[0]] = (row[1], row[2])
 
-    stream = prodigy.components.loaders.TXT(source)
+    file_name = r"{0}.txt".format(recipe_number)
+    stream = prodigy.components.loaders.TXT(Path(source_dir, file_name))
     stream = [prodigy.util.set_hashes(eg) for eg in stream]
-    stream = (eg for score, eg in model(stream))
+    # stream = (eg for score, eg in model(stream))
+    for i, elem in enumerate(stream):
+        spans = []
+        for entity in entities_list[recipe_number].values():
+            if entity.text in elem['text']:
+                span = {'start': entity.span[0][0], 'end': entity.span[0][1], 'text': entity.text, 'label': entity.type}
+                spans.append(span)
+        stream[i]['spans'] = spans
 
     stream = _add_option(stream, kb, id_dict)
     stream = prodigy.components.filters.filter_duplicates(stream, by_input=True, by_task=False)
@@ -50,7 +72,12 @@ def _add_option(stream, kb, id_dict):
             end_char = int(span["end"])
             mention = text[start_char:end_char]
 
-            candidates = kb.get_alias_candidates(mention)
+            candidates = []
+            for elem in mention.split(' '):
+                res = kb.get_alias_candidates(elem)
+                if res:
+                    candidates.append(res[0])
+
             if candidates:
                 options = [{"id": c.entity_, "html": _print_url(c.entity_, id_dict)} for c in candidates]
                 options = sorted(options, key=lambda r: r["id"])
@@ -63,5 +90,9 @@ def _add_option(stream, kb, id_dict):
 def _print_url(entity_id, id_dict):
     url_prefix = "https://www.wikidata.org/wiki"
     name, descr = id_dict.get(entity_id)
-    option = "<a href='" + url_prefix + entity_id + "'>" + entity_id + "</a>: " + name + ':' + descr
+    option = "<a href='" + url_prefix + entity_id + "'>" + entity_id + "</a>: " + name
+
+    if descr and descr != 'NONE':
+        option = option + ':' + descr
+
     return option
